2eb512dcbd05e8477bc0bd5ec2a1345162d0f224
Marcin Siemiński <marcin.sieminski@outlook.com>
2021-09-13 22:08:33 +0200

    Add value controller action methods

--- Portal.API/Controllers/ValueController.cs
+++ Portal.API/Controllers/ValueController.cs
@@ -1,8 +1,61 @@
 using Microsoft.AspNetCore.Mvc;
+using Portal.API.Data;
+using Portal.API.Models;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Portal.API.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class ValueController : ControllerBase
     {
-        
+        private DataContext _context;
+
+        public ValueController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetValues()
+        {
+            var values = _context.Values.ToList();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetValue(int id)
+        {
+            var value = _context.Values.FirstOrDefault(x => x.Id == id);
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult AddValue([FromBody] Value value)
+        {
+            _context.Values.Add(value); 
+            _context.SaveChanges();
+            return Ok(value);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult EditValue(int id, [FromBody] Value value)
+        {
+            var data = _context.Values.Find(id);
+            data.Name = value.Name;
+            _context.Values.Update(data);
+            _context.SaveChanges();
+            return Ok(data);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteValue(int id)
+        {
+            var data = _context.Values.Find(id);
+            _context.Values.Remove(data);
+            _context.SaveChanges();
+            return Ok(data);
+        }
     }
 }
