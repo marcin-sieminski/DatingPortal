530ed9ebd90733c156e095149322638aad266410
Marcin Siemiński <marcin.sieminski@outlook.com>
2021-09-16 23:00:41 +0200

    Add value component

--- Portal.SPA/src/app/app.module.ts
+++ Portal.SPA/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ValueComponent } from './value/value.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      ValueComponent
+   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
