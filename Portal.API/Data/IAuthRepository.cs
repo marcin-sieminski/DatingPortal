6b805a75ab6b55b537641fb9973cba21cee64c40
Marcin Siemiński <marcin.sieminski@outlook.com>
2021-10-02 18:32:25 +0200

    Implement register user method

--- Portal.API/Data/IAuthRepository.cs
+++ Portal.API/Data/IAuthRepository.cs
@@ -6,7 +6,7 @@ namespace Portal.API.Data
     public interface IAuthRepository
     {
         Task<User> Login(string username, string password);
-        Task<User> Register(string user, string password);
+        Task<User> Register(User user, string password);
         Task<bool> UserExists(string username);
     }
 }
