7f07e1933d74f61b7d6777e3e4f8e5a704940db1
Marcin Siemiński <marcin.sieminski@outlook.com>
2021-10-02 18:44:38 +0200

    Implement login user method

--- Portal.API/Data/AuthRepository.cs
+++ Portal.API/Data/AuthRepository.cs
@@ -1,6 +1,6 @@
-using System;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Portal.API.Models;
 
 namespace Portal.API.Data
@@ -14,9 +14,20 @@ namespace Portal.API.Data
             _context = context;
         }
 
-        public Task<User> Login(string username, string password)
+        public async Task<User> Login(string username, string password)
         {
-            throw new System.NotImplementedException();
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Username == username);
+            if (user is null)
+            {
+                return null;
+            }
+
+            if (!VerifyPasswordHash(password, user.PasswordHash, user.PasswordSalt))
+            {
+                return null;
+            };
+
+            return user;
         }
 
         public async Task<User> Register(User user, string password)
@@ -28,23 +39,43 @@ namespace Portal.API.Data
 
             await _context.Users.AddAsync(user);
             await _context.SaveChangesAsync();
-            
+
             return user;
         }
 
-        public Task<bool> UserExists(string username)
+        public async Task<bool> UserExists(string username)
         {
-            throw new System.NotImplementedException();
+            if (await _context.Users.AnyAsync(x => x.Username == username))
+            {
+                return true;
+            }
+            return false;
         }
         #endregion
-        
+
         #region method privat
         private void CreatePasswordHashSalt(string password, out byte[] passwordHash, out byte[] passwordSalt)
         {
-            using(var hmac = new System.Security.Cryptography.HMACSHA512())
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
             {
                 passwordSalt = hmac.Key;
                 passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+                for (int i = 0; i < computedHash.Length; i++)
+                {
+                    if (computedHash[i] != passwordHash[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
             }
         }
         #endregion
