e2a55d283f8a89cbc0aad7945b2d180dbbede823
Marcin Siemiński <marcin.sieminski@outlook.com>
2021-10-10 16:54:33 +0200

    Implement login using JWT

--- Portal.API/Controllers/AuthController.cs
+++ Portal.API/Controllers/AuthController.cs
@@ -1,8 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using Portal.API.Data;
 using Portal.API.Dtos;
 using Portal.API.Models;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using System;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace Portal.API.Controllers
 {
@@ -11,9 +17,12 @@ namespace Portal.API.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repository;
-        public AuthController(IAuthRepository repository)
+        private readonly IConfiguration _config;
+
+        public AuthController(IAuthRepository repository, IConfiguration config)
         {
             _repository = repository;
+            _config = config;
         }
 
         [HttpPost("register")]
@@ -25,9 +34,37 @@ namespace Portal.API.Controllers
                 return BadRequest("U¿ytkownik o takiej nazwie istnieje.");
             }
 
-            var userToCreate = new User(){Username = userForRegisterDto.Username};
+            var userToCreate = new User() { Username = userForRegisterDto.Username };
             var createdUser = await _repository.Register(userToCreate, userForRegisterDto.Password);
             return Ok(createdUser);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepository = await _repository.Login(userForLoginDto.UserName.ToLower(), userForLoginDto.Password);
+            if (userFromRepository == null)
+            {
+                return Unauthorized();
+            }
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepository.Id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepository.Username)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddHours(12),
+                SigningCredentials = creds
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return Ok(new { token = tokenHandler.WriteToken(token) });
         }
     }
 }
---
+++ Portal.API/Dtos/UserForLoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Portal.API.Dtos
+{
+    public class UserForLoginDto
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
