fd097d219d67b27100d7865044d293bd61d6db34
Marcin Siemiński <marcin.sieminski@outlook.com>
2021-11-14 08:50:00 +0100

    Add detailed user info

--- Portal.API/Models/User.cs
+++ Portal.API/Models/User.cs
@@ -8,7 +8,38 @@ public class User
     public byte[] PasswordSalt { get; set; }
 
     public string Gender { get; set; }
-    public DateOnly MyProperty { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public string ZodiacSign { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime LastActive { get; set; }
+    public string City { get; set; }
+    public string Country { get; set; }
 
+    public int Growth { get; set; }
+    public string EyeColor { get; set; }
+    public string HairColor { get; set; }
+    public string MartialStatus { get; set; }
+    public string Education { get; set; }
+    public string Profession { get; set; }
+    public string Children { get; set; }
+    public string Language { get; set; }
 
+    public string Motto { get; set; }
+    public string Description { get; set; }
+    public string Personality { get; set; }
+    public string LookingFor { get; set; }
+
+    public string Interests { get; set; }
+    public string FreeTime { get; set; }
+    public string Sport { get; set; }
+    public string Movies { get; set; }
+    public string Music { get; set; }
+
+    public string ILike { get; set; }
+    public string IDoNotLike { get; set; }
+    public string MakesMeLaugh { get; set; }
+    public string ItFeelsBestIn { get; set; }
+    public string FriendsWouldDescribeMe { get; set; }
+
+    public ICollection<Photo> Photos { get; set; }
 }
