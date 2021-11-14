93f7bc7ceb83c4f81455e3cd2236bfc5b5cf28fa
Marcin Siemiński <marcin.sieminski@outlook.com>
2021-11-14 08:23:38 +0100

    Enable implicit usings

--- Portal.API/Models/User.cs
+++ Portal.API/Models/User.cs
@@ -1,11 +1,14 @@
-namespace Portal.API.Models
+namespace Portal.API.Models;
+
+public class User
 {
-    public class User
-    {
-        public int Id { get; set; }
-        public string Username { get; set; }
-        public byte[] PasswordHash { get; set; }
-        public byte[] PasswordSalt
-        { get; set; }
-    }
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
+
+    public string Gender { get; set; }
+    public DateOnly MyProperty { get; set; }
+
+
 }
