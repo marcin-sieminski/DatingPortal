609a55f238320eb33e88fcf913a7dbd4629074ee
Marcin Siemiński <marcin.sieminski@outlook.com>
2021-12-05 07:47:30 +0100

    Add users controller

---
+++ Portal.API/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Portal.API.Data;
+using System.Threading.Tasks;
+
+namespace Portal.API.Controllers;
+
+[Authorize]
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    private readonly IUserRepository _repo;
+
+    public UsersController(IUserRepository repo)
+    {
+        _repo = repo;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetUsers()
+    {
+        return Ok(await _repo.GetUsers());
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetUser(int id)
+    {
+        return Ok(await _repo.GetUser(id));
+    }
+}
